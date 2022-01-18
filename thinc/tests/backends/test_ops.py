2a6df855b2c5fe6403fdd324228cbdc17678ccbe
Daniël de Kok <me@danieldk.eu>
2022-01-18 14:38:53 +0100

    Add test for backprop_seq2col with lengths + window 2

--- thinc/tests/backends/test_ops.py
+++ thinc/tests/backends/test_ops.py
@@ -307,6 +307,26 @@ def test_seq2col_window_two(ops):
     assert_allclose(cols[3], [2.0, 3.0, 4.0, 0.0, 0.0])
 
 
+@pytest.mark.parametrize("ops", [NumpyOps()])
+def test_backprop_seq2col_window_two_lens(ops):
+    d_y = ops.xp.arange(0.1, 7.6, step=0.1, dtype="float32").reshape(5, 15)
+    lens = ops.asarray1i([1, 3, 1])
+    d_seqs = ops.backprop_seq2col(d_y, 2, lens)
+
+    ops.xp.testing.assert_allclose(
+        ops.asarray2f(
+            [
+                [0.7, 0.8, 0.9],
+                [10.2, 10.5, 10.8],
+                [11.1, 11.4, 11.7],
+                [12.0, 12.3, 12.6],
+                [6.7, 6.8, 6.9],
+            ]
+        ),
+        d_seqs,
+    )
+
+
 @pytest.mark.parametrize("ops", XP_OPS)
 def test_backprop_seq2col_window_two(ops):
     cols = ops.asarray(
